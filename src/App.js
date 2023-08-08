c9c03c7a91bb998aec2794c95c353adc941b2b1c
Suryasnata Saha <suryasnata2303@gmail.com>
2023-08-08 10:46:47 +0530

    All button functionalities added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./App.css";
 
 function App() {
-  return <div>App</div>;
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editTodo, setEditTodo] = useState(null);
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form
+            input={input}
+            setInput={setInput}
+            todos={todos}
+            setTodos={setTodos}
+            editTodo={editTodo}
+            setEditTodo={setEditTodo}
+          />
+        </div>
+        <div>
+          <TodoList
+            todos={todos}
+            setTodos={setTodos}
+            setEditTodo={setEditTodo}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
